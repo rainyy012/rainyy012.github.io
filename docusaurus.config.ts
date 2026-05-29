6fb3a4b3b2f1114fe9c4694542a4538de7d882a6
Rain <rainyy-012@proton.me>
2026-05-30 04:36:37 +0800

    refactor: temporarily disabled tip page

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -161,19 +161,19 @@ const config: Config = {
             },
           ],
         },
-        {
-          // title: '',
-          items: [
-            {
-              label: 'Support this site',
-              to: '/tip',
-            },
-            // {
-            //   label: 'Discord',
-            //   to: DISCORD_LINK,
-            // },
-          ],
-        },
+        // {
+        //   // title: '',
+        //   items: [
+        //     {
+        //       label: 'Support this site',
+        //       to: '/tip',
+        //     },
+        //     // {
+        //     //   label: 'Discord',
+        //     //   to: DISCORD_LINK,
+        //     // },
+        //   ],
+        // },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Rain.`,
     },
