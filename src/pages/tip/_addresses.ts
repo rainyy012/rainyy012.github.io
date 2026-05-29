8f98f8d95c87ad74abb518270f0655930c8dafaa
Rain <rainyy-012@proton.me>
2026-05-30 06:01:14 +0800

    chore: code optimizations

--- src/pages/tip/_addresses.ts
+++ src/pages/tip/_addresses.ts
@@ -1,10 +1,15 @@
 import { CUSTOM_VALUES } from '@site/src/components/custom-value'
 import seedrandom from 'seedrandom'
 
+const missingValues: Array<string> = []
 export const BTC_ADDRESS = CUSTOM_VALUES.DONATE_BTC_ADDRESSES[
   Math.floor(CUSTOM_VALUES.DONATE_BTC_ADDRESSES.length * seedrandom(
     String(5 * Math.floor(new Date().getDate() / 5))
   )())
 ]
+if (!BTC_ADDRESS) { missingValues.push('BTC_ADDRESS') }
 
 export const ETH_ADDRESS = CUSTOM_VALUES.DONATE_ETH_ADDRESS
+if (!BTC_ADDRESS) { missingValues.push('ETH_ADDRESS') }
+
+throw new Error(`Missing values: ${missingValues.join(', ')}`)
