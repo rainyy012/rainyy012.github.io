fb2915ce3628f3de8d3447ce46e7d5824ef8c12c
Rain <rainyy-012@proton.me>
2026-05-31 02:48:49 +0800

    bulk: misc rephrasing, ui adjustments

--- src/components/yt-embed/index.tsx
+++ src/components/yt-embed/index.tsx
@@ -1,5 +1,6 @@
 import { useWindowSize } from '@docusaurus/theme-common'
 import { ReactNode } from 'react'
+import styles from './index.module.css'
 
 export interface YTEmbedProps {
   videoId: string
@@ -10,14 +11,16 @@ export function YTEmbed({
 }: YTEmbedProps): ReactNode {
   const windowSize = useWindowSize()
   return (
-    <iframe
-      {...(windowSize === 'desktop' ? { height: 315, width: 560 } : {})}
-      src={`https://www.youtube.com/embed/${videoId}`}
-      title="YouTube video player"
-      frameBorder={0}
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerPolicy="strict-origin-when-cross-origin"
-      allowFullScreen
-    />
+    <div className={styles.container}>
+      <iframe
+        {...(windowSize === 'desktop' ? { height: 315, width: 560 } : {})}
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        frameBorder={0}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      />
+    </div>
   )
 }
