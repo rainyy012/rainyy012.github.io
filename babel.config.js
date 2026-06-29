3d18f455ae481f46f3ed118a8a46838fd32151c7
Rain <rainyy-012@proton.me>
2026-06-29 14:19:33 +0800

    refactor: babel config

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+export default {
+  env: {
+    test: {
+      presets: [
+        '@babel/preset-typescript',
+        ['@babel/preset-react', {
+          runtime: 'automatic',
+        }],
+      ],
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+      ],
+    },
+  },
+}
